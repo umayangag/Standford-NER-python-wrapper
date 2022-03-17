8ad5278ce0120bef767fbce005b00db6aae6af68
umayanga <umayangag@gmail.com>
2022-03-18 01:44:30 +0530

    generate requirements for nltk

--- classify.py
+++ classify.py
@@ -6,8 +6,10 @@ from flask import Flask
 from flask_restful import Api, Resource
 from flask import request
 from waitress import serve
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 st = StanfordNERTagger('stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
