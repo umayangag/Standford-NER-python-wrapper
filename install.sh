793c3469031eff12c13c628a5b1a70a0fc549845
umayanga <umayangag@gmail.com>
2022-03-25 18:08:09 +0530

    modify install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
-virtualenv -p /usr/bin/python3.8 venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install nltk flask flask_restful waitress
-wget https://nlp.stanford.edu/software/stanford-ner-2018-10-16.zip
-unzip stanford-ner-2018-10-16.zip
-mv stanford-ner-2018-10-16/ stanford-ner
-rm stanford-ner-2018-10-16.zip
+sudo apt-get install python3-pip &&
+sudo pip3 install virtualenv &&
+virtualenv -p /usr/bin/python3.6 venv &&
+source venv/bin/activate &&
+pip3 install --upgrade pip &&
+pip3 install -r requirements.txt &&
+pip3 install nltk flask flask_restful waitress &&
+wget https://nlp.stanford.edu/software/stanford-ner-2018-10-16.zip &&
+unzip stanford-ner-2018-10-16.zip &&
+mv stanford-ner-2018-10-16/ stanford-ner &&
+rm stanford-ner-2018-10-16.zip &&
 python3 -m nltk.downloader -d venv/nltk_data punkt
